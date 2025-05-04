4c554008507832d361b0257196d96f37d7f2b90d
Milad Naeini <milad88884@gmail.com>
2025-05-04 15:11:45 +0100

    feat: design homepage with CSS and DOM manipulation to adjust heights: -Designed the homepage layout according to the provided design specifications. -Used CSS for styling various elements, including the header, sidebar, and main content. - implemented DOM manipulation logic to dynamically adjust the heights of sidebar elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,71 @@
-// "use client";
+"use client";
+import { useEffect } from "react";
 import data from "./data/data.json";
 import Image from "next/image";
 import vaultImage from "../../public/images/NordGen.webp";
 
 export default function Home() {
-  return (
-    <main>
-      <div className="main-card">
-        <Image src={vaultImage} alt="vault" />
-        <h1 className="heading">
-          {data.title}
-        </h1>
-        <div>
-          By {data.author} . {data.category}
+
+    useEffect(() => {
+      if (!!document) {
+        const titleElement = document.querySelector("#title-element") as HTMLDivElement;
+        const firstSidebarElement = document.querySelector(
+          "#first-sidebar-element"
+        ) as HTMLDivElement;
+        const preludeElement = document.querySelector("#prelude-element") as HTMLDivElement;
+        const secondSidebarElement = document.querySelector(
+          "#second-sidebar-element"
+        ) as HTMLDivElement;
+  
+        firstSidebarElement.style.height = `${firstSidebarElement?.clientHeight +
+         titleElement?.clientHeight + 16}px`;
+        preludeElement.style.height = `${preludeElement?.clientHeight +
+         firstSidebarElement?.clientHeight + 16}px`;
+        secondSidebarElement.style.height = `${
+         (preludeElement?.clientHeight - firstSidebarElement?.clientHeight) +
+        secondSidebarElement?.clientHeight}px`;
+      }
+    }, []);
+    
+  return  (
+    <div className="article-container">
+      <div className="top-container">
+        <div className="sidebar">
+          <div id="first-sidebar-element">
+          By {data.author} · {data.category} - {data.publishedAt}
+          </div>
+          <div id="second-sidebar-element">
+          {data.paragraphs[0]}
+          </div>
+          <div>
+          {data.paragraphs[1]}
+          </div>
         </div>
-        <div>
-          {data.publishedAt}
-        </div>
-        <p className="prelude">
+        <div className="header">
+          <h1 id="title-element">
+            {data.title}
+          </h1>
+          <div id="prelude-element" className="prelude">
           {data.prelude}
-        </p>
-        <p className="paragraph">
-          {data.paragraphs}
-        </p>
+          </div>
+          <div className="image-container">
+          <Image
+            src={vaultImage}
+            alt=""
+          />
+          </div>
+        </div>
       </div>
-    </main>
+
+      <div className="bottom-container">
+      {data.paragraphs.slice(2).map((para, idx) =>
+         <div key={idx}>
+            <p >
+              {para}
+            </p>
+         </div>
+          )}
+      </div>
+    </div>
   );
 }
